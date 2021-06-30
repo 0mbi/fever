99131c6f8b6fbeb58b6b2b924d58b1cecc7a5491
Markus Hihn <markus.hihn@dcso.de>
2021-06-30 22:17:36 +0200

    MAGIT autocommit /home/0mbi/go/src/github.com/DCSO/fever/input/input.go

--- input/input.go
+++ input/input.go
@@ -3,8 +3,8 @@ package input
 // DCSO FEVER
 // Copyright (c) 2017, DCSO GmbH
 
-// Input is an interface describing the behaviour for a component to
-// handle events parsed from EVE input.
+// Input describes how to handle
+// events parsed from EVE input.
 type Input interface {
 	GetName() string
 	Run()
